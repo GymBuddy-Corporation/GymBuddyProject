273c64ffc99baef0854cd3ef3cf09cbd145ef868
MartorelliLuca <lucam0109@gmail.com>
2023-10-13 15:42:33 +0200

    Solved radiobutton bug on GymUserRegistrationGUI.java

--- src/main/java/viewone/graphical_controllers/gym/GymUserRegistrationGUI.java
+++ src/main/java/viewone/graphical_controllers/gym/GymUserRegistrationGUI.java
@@ -4,47 +4,33 @@ import utils.MainStage;
 import utils.SwitchPage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GymUserRegistrationGUI implements Initializable {
 
-    @FXML RadioButton PersonalTrainer;
-    @FXML RadioButton Athlete;
     @FXML RadioButton Male;
     @FXML RadioButton Female;
 
-    @FXML public void goForward(ActionEvent event) throws Exception {
+    @FXML public void goForward() throws Exception {
         String path = "/viewone/gym/GymHome.fxml";
         SwitchPage.setStage(MainStage.stage,path);
     }
 
-    @FXML public void goBack(ActionEvent event) throws Exception {
+    @FXML public void goBack() throws Exception {
         String path = "/viewone/gym/GymUserView.fxml";
         SwitchPage.setStage(MainStage.stage,path);
     }
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ToggleGroup group1 = new ToggleGroup();
-        ToggleGroup group2 = new ToggleGroup();
         Male.setToggleGroup(group1);
         Female.setToggleGroup(group1);
         Male.fire();
-        Athlete.setToggleGroup(group2);
-        PersonalTrainer.setToggleGroup(group2);
-        Athlete.fire();
-
     }
 }
