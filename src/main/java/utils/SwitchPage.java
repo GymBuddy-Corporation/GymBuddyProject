36b8dc612207c0802dc1c2faaa75ada89fbefffa
GigaChad0307030 <alexandru.nazare@students.uniroma2.eu>
2023-11-08 10:13:02 +0100

    Changed switchPage to follow the uri standard(??)

--- src/main/java/utils/SwitchPage.java
+++ src/main/java/utils/SwitchPage.java
@@ -5,7 +5,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+
+import static java.lang.System.exit;
 
 public class SwitchPage {
 
@@ -14,8 +17,27 @@ public class SwitchPage {
     }
     static int x=800;
     static int y=500;
-    public static  Scene setStage(Stage stage, String toscene) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SwitchPage.class.getResource(toscene));
+
+    private static String SEP="/";
+    private static String VIEWONE="viewone";
+    public static  Scene setStage(Stage stage, String path){
+        return null;
+    }
+    public static  Scene setStage(Stage stage, String name,String folder,int view) throws IOException {
+        String path;
+        String view_path="";
+        switch (view){
+            case 1:
+                view_path=VIEWONE;
+                break;
+
+            default:
+                //TODO:trhoware un exception quando capiro come fare
+                exit(0);
+            break;
+        }
+        path=SEP+view_path+SEP+folder+SEP+name;
+        FXMLLoader fxmlLoader = new FXMLLoader(SwitchPage.class.getResource(path));
         Parent load = fxmlLoader.load();
         Scene scena = new Scene(load, x, y);
         stage.setResizable(false);
--- src/main/java/viewone/GymBuddy.java
+++ src/main/java/viewone/GymBuddy.java
@@ -11,7 +11,7 @@ public class GymBuddy extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         MainStage.setStage(stage); //Va fatto una sola volta siccome è static, quindi una volta lanciata l'applicazione non serve cambiarlo
-        SwitchPage.setStage(MainStage.getStage(),"/viewone/launcher/hello-viewone.fxml");
+        SwitchPage.setStage(MainStage.getStage(),"hello-viewone.fxml","launcher",1);
     }
 
     public static void main(String[] args) {
