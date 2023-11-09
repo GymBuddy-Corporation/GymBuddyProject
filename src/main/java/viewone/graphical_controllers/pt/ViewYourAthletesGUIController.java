8fc8a830fe3f8c02a62ef43201927d270203e8fa
MartorelliLuca <lucam0109@gmail.com>
2023-11-09 11:55:43 +0100

    added list view

---
+++ src/main/java/viewone/graphical_controllers/pt/ViewYourAthletesGUIController.java
@@ -0,0 +1,34 @@
+package viewone.graphical_controllers.pt;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import utils.MainStage;
+import utils.SwitchPage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ViewYourAthletesGUIController implements Initializable {
+
+    @FXML
+    private ListView<String> athletesList;
+
+    @FXML
+    public void goBack() throws Exception {
+        SwitchPage.setStage(MainStage.getStage(),"PTHome.fxml","pt",1);
+    }
+    public void logout() throws Exception {
+        SwitchPage.setStage(MainStage.getStage(),"PTLogin.fxml","launcher",1);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<String> lista = new ArrayList<>(Arrays.asList("Luca Martorelli", "Matteo Martorelli", "Marco Martorelli", "Alexandru Nazare", "Alessandro Lori", "Alessandro Cortese", "Giulia Boccuccia", "Andrea De Filippis"));
+        athletesList.getItems().addAll(lista);
+    }
+}
