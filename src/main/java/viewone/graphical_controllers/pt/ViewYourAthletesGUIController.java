03d0f6a0968504f10a178624abb88793a7924e25
MartorelliLuca <lucam0109@gmail.com>
2023-11-09 15:05:51 +0100

    added AddAthletesPTListCellFactory.java class that will be very usefull for the managing of future list views (We need to know how to manage beans first).

--- src/main/java/viewone/graphical_controllers/pt/ViewYourAthletesGUIController.java
+++ src/main/java/viewone/graphical_controllers/pt/ViewYourAthletesGUIController.java
@@ -1,21 +1,33 @@
 package viewone.graphical_controllers.pt;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 import utils.MainStage;
 import utils.SwitchPage;
+import viewone.utilities.AddAthletesPTListCellFactory;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
-public class ViewYourAthletesGUIController implements Initializable {
+public class ViewYourAthletesGUIController implements Initializable{
 
     @FXML
+    /*private ListView<EventBean> dashboard;
+
+    private final ArrayList<EventBean> elements= new ArrayList<>();*/
     private ListView<String> athletesList;
+
+    /*private void clear(){
+        elements.clear();
+    }
+    private void refresh(){
+        dashboard.refresh();
+    }*/
 
     @FXML
     public void goBack() throws Exception {
@@ -28,7 +40,42 @@ public class ViewYourAthletesGUIController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<String> lista = new ArrayList<>(Arrays.asList("Luca Martorelli", "Matteo Martorelli", "Marco Martorelli", "Alexandru Nazare", "Alessandro Lori", "Alessandro Cortese", "Giulia Boccuccia", "Andrea De Filippis"));
+
+        /*clear();
+        refresh();
+        List<EventBean> y;*/
+        /*y = new ItemController().item("");*/
+
+        /*while(!(y.isEmpty())){
+            EventBean z= y.get(0);
+            elements.add(z);
+            y.remove(0);
+        }*/
+
+
+        /*dashboard.setItems(FXCollections.observableList(elements));
+
+        dashboard.setCellFactory(param -> new AddAthletesPTListCellFactory());
+        dashboard.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            Stage stage = new Stage();
+            FXMLLoader root;
+            root = new FXMLLoader(Objects.requireNonNull(getClass().getResource("ViewYourAthletes.fxml")));
+            String x= elements.get(dashboard.getSelectionModel().getSelectedIndex());
+
+            Scene scene;
+            scene = new Scene(root.load(), 417, 359);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+                JoinEventGraphicController o = root.getController();
+                o.setDescriptionText(x);
+            assert false;
+
+        });*/
+        List<String> lista = new ArrayList<>(Arrays.asList("Luca Martorelli", "Matteo Martorelli",
+                "Marco Martorelli", "Alexandru Nazare", "Alessandro Lori", "Alessandro Cortese",
+                "Giulia Boccuccia", "Andrea De Filippis", "Edoardo Manenti", "Chiara Iurato", "Simone Thanos"));
         athletesList.getItems().addAll(lista);
     }
 }
