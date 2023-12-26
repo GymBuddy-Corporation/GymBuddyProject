9d5a95c53d7ff14b74d1c71a24a3de3ee3493ef4
Luca Martorelli <92364829+MartorelliLuca@users.noreply.github.com>
2023-12-26 18:34:36 +0100

    Nothing important, just started to let the athlete create the request.

--- src/main/java/viewone/graphicalControllers/athlete/AskNewRoutineGUIController.java
+++ src/main/java/viewone/graphicalControllers/athlete/AskNewRoutineGUIController.java
@@ -1,29 +1,26 @@
 package viewone.graphicalControllers.athlete;
 
-
 import javafx.fxml.FXML;
-
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
 import utils.MainStage;
 import utils.SwitchPage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-public class AthletesRoutineGUIController implements Initializable {
-
-    @FXML
-    private ListView<String> athletesList;
+public class AskNewRoutineGUIController implements Initializable {
 
     @FXML
     public void goBack() throws Exception {
-        SwitchPage.setStage(MainStage.getStage(),"AthleteHome.fxml","athlete",1);
+        SwitchPage.setStage(MainStage.getStage(),"AthletesRoutine.fxml","athlete",1);
     }
     public void logout() throws Exception {
         SwitchPage.setStage(MainStage.getStage(),"AthleteLogin.fxml","launcher",1);
     }
+    public void askRequest() throws Exception {
+        SwitchPage.setStage(MainStage.getStage(),"AthleteHome.fxml","launcher",1);
+    }
+
     @FXML
     public void askForNewRoutine() {
         //TODO gestisci la rischiesta di una nuova scheda
